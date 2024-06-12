e9dfed5529b75072d787f2e2acf3c676968f4eaa
Kashish2908 <kneekhra29@gmail.com>
2024-06-12 18:03:01 +0530

    made ui changes , in login page and integrated state persistence for datagrid

--- final-aerialview/wwwroot/js/site.js
+++ final-aerialview/wwwroot/js/site.js
@@ -76,5 +76,36 @@ sidebarToggleHandling();
 menuToggleHandling();
 subMenuToggleHandling();
 
+//reset state persistence
+function onStateResetClick() {
+    const dataGrid = $("#dataGridContainer").dxDataGrid("instance");
+    dataGrid.state(null);
+}
 
+
+
+//animating login page
+document.addEventListener("DOMContentLoaded", function () {
+    gsap.from(".login-wrapper", { duration: 1, opacity: 0, y: -50, ease: "power2.out" });
+
+    gsap.from(".logo-img", { duration: 1, opacity: 0, x: -100, scale: 0.5, ease: "power2.out" });
+
+    gsap.from(".img-part img", { duration: 1, opacity: 0, x: 100, ease: "power2.out" });
+
+    gsap.from(".login-font", { duration: 1, opacity: 0, x: -100, ease: "power2.out" });
+
+    gsap.from(".form-floating", { duration: 1, opacity: 0, x: -100, stagger: 0.2, ease: "power2.out" });
+
+    gsap.from("#login-submit", { duration: 1, opacity: 0, x: -100, ease: "power2.out", delay: .2 });
+
+    gsap.from(".copyright-text", { duration: 1, opacity: 0, x: -100, ease: "power2.out", delay: .2 });
+
+
+
+    //gsap.from(".login-font", { duration: .5, opacity: 0, x: -100, delay: 1, ease: "power2.out" });
+    //gsap.from(".form-floating", { duration: .6, opacity: 0, x: 20, stagger: 0.2, delay: 1.5 });
+    //gsap.from("#login-submit", { duration: .6, opacity: 0, scale: 0.8, delay: 2 });
+
+    //gsap.from(".copyright-text", { duration: .5, opacity: 0, delay: 2, ease: "easeInOut" })
+});
 
