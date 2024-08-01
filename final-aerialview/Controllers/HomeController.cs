5af88b8da35cf17a4626b9c25c3028ab36b500df
Kashish2908 <kneekhra29@gmail.com>
2024-08-01 14:32:39 +0530

    change pdf exporting logic

--- final-aerialview/Controllers/HomeController.cs
+++ final-aerialview/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using final_aerialview.Data;
 using final_aerialview.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 
@@ -13,34 +14,29 @@ namespace final_aerialview.Controllers
         [Authorize]
         public IActionResult Index()
         {
-
-            //// Check if session flag is already set
-            //bool isNewTabOpened = HttpContext.Session.GetString("IsNewTabOpened") == "True";
-
-
-            //// Pass flag to view
-            //ViewBag.IsNewTabOpened = isNewTabOpened;
-
-
-            //return View();
-
-            if (HttpContext.Session.GetString("IsNewTabOpened") == null)
-            {
-                HttpContext.Session.SetString("IsNewTabOpened", "True");
-                ViewBag.IsNewTabOpened = "False";
-            }
-            else
-            {
-                ViewBag.IsNewTabOpened = "True";
-            }
+            //if (HttpContext.Session.GetString("IsNewTabOpened") == null)
+            //{
+            //    HttpContext.Session.SetString("IsNewTabOpened", "True");
+            //    ViewBag.IsNewTabOpened = "False";
+            //}
+            //else
+            //{
+            //    ViewBag.IsNewTabOpened = "True";
+            //}
             return View();
         }
+
+       
+
+
 
         public IActionResult NoReportAvailable(int datagridRptid)
         {
             ViewBag.DatagridRptid = datagridRptid;
             return View();
         }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
