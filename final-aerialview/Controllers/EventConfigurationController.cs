65f72fdcea1942a359c483a04b74488c3dd7eb8d
Kashish2908 <kneekhra29@gmail.com>
2024-10-14 18:05:54 +0530

    added one more menu item and a page for alarm and event configuration

---
+++ final-aerialview/Controllers/EventConfigurationController.cs
@@ -0,0 +1,22 @@
+﻿using DevExpress.ReportServer.ServiceModel.DataContracts;
+using final_aerialview.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace final_aerialview.Controllers
+{
+    public class EventConfigurationController(DataAccess dataAccess) : BaseController(dataAccess)
+    {
+        public IActionResult EventConfig()
+        {
+            var connTableData = _dataAccess.GetChildMenuData();
+            var reports = connTableData
+                .GroupBy(r => r.stringName)
+                .Select(g => g.First())
+                .ToList();
+
+            ViewData["Reports"] = reports;
+
+            return View();
+        }
+    }
+}
