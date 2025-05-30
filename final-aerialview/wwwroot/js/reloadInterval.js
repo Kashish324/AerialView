67ad3c984433d037cfc1d0ef55fac552ae31520f
Kashish324 <kashish.neekhra@himak.in>
2025-05-30 17:05:35 +0530

    kept the dashboard reload function in single js file

---
+++ final-aerialview/wwwroot/js/reloadInterval.js
@@ -0,0 +1,29 @@
+﻿var dashboardControl; // Declare globally
+var refreshIntervalId;
+
+
+function onBeforeRender() {
+    // Initialize or capture the dashboard control
+    dashboardControl = clientDashboardControl;
+    var refreshRate = document.getElementById('dashboard-wrapper').getAttribute('data-refresh-rate');
+
+    if (isNaN(refreshRate) || refreshRate <= 0) {
+        console.log("Auto-refresh disabled (refreshRate is 0 or invalid).");
+        return; // Exit early
+    }
+
+    if (refreshRate < 1000) {
+        refreshRate *= 1000;
+    }
+
+    if (refreshIntervalId) {
+        clearInterval(refreshIntervalId)
+    }
+
+    refreshIntervalId = setInterval(() => {
+        if (dashboardControl) {
+            dashboardControl.reloadData();
+            console.log("Dashboard refreshed");
+        }
+    }, refreshRate);
+}
